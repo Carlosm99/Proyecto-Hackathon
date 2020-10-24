bf7042cfd123a9068cc15c579f873e381494f476
carlos monterrosa <carlosmontess07@gmail.com>
2020-10-24 01:24:17 -0500

    another move

--- apcapa/src/environments/environment.ts
+++ apcapa/src/environments/environment.ts
@@ -6,6 +6,17 @@ export const environment = {
   production: false
 };
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyBS4PObgykxjeXEVAVLYt10AghqyRsN1oU",
+  authDomain: "capa-b3a87.firebaseapp.com",
+  databaseURL: "https://capa-b3a87.firebaseio.com",
+  projectId: "capa-b3a87",
+  storageBucket: "capa-b3a87.appspot.com",
+  messagingSenderId: "1033020631400",
+  appId: "1:1033020631400:web:5947619cd10ddb4b1fcedd",
+  measurementId: "G-KTRZV8VQCQ"
+};
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
