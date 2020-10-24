679057409809f858b866d9b549a3f0972d8bbbad
carlos monterrosa <carlosmontess07@gmail.com>
2020-10-24 02:18:40 -0500

    adding the base

--- apcapa/src/app/componentes/login/login.page.ts
+++ apcapa/src/app/componentes/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from "../../servicios/auth.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  email: string;
+  password: string;
+
+  constructor(private AuthService: AuthService, public router: Router) { }
 
   ngOnInit() {
   }
+  onSubmitlogin()
+  {
+    this.AuthService.login(this.email, this.password).then( res =>{
+      this.router.navigate(['/folder/Inbox']);
+    }).catch(err => alert('contraseña o correo error'))
+  }
 
-}
+  }
+
