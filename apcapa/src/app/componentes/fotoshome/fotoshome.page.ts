209da80df21be89bc8bc8a0fa6e5825f266608d3
carlos monterrosa <carlosmontess07@gmail.com>
2020-10-27 16:54:31 -0500

    chmod

--- apcapa/src/app/componentes/fotoshome/fotoshome.page.ts
+++ apcapa/src/app/componentes/fotoshome/fotoshome.page.ts
@@ -63,6 +63,110 @@ export class FotoshomePage implements OnInit {
     toast.present();
   }
 
-  
+  async selectImage() {
+    const actionSheet = await this.actionSheetController.create({
+        header: "Select Image source",
+        buttons: [{
+                text: 'Load from Library',
+                handler: () => {
+                    this.takePicture(this.camera.PictureSourceType.PHOTOLIBRARY);
+                }
+            },
+            {
+                text: 'Use Camera',
+                handler: () => {
+                    this.takePicture(this.camera.PictureSourceType.CAMERA);
+                }
+            },
+            {
+                text: 'Cancel',
+                role: 'cancel'
+            }
+        ]
+    });
+    await actionSheet.present();
+}
+ 
+takePicture(sourceType: PictureSourceType) {
+    var options: CameraOptions = {
+        quality: 100,
+        sourceType: sourceType,
+        saveToPhotoAlbum: false,
+        correctOrientation: true
+    };
+ 
+    this.camera.getPicture(options).then(imagePath => {
+        if (this.platform.is('android') && sourceType === this.camera.PictureSourceType.PHOTOLIBRARY) {
+            this.filePath.resolveNativePath(imagePath)
+                .then(filePath => {
+                    let correctPath = filePath.substr(0, filePath.lastIndexOf('/') + 1);
+                    let currentName = imagePath.substring(imagePath.lastIndexOf('/') + 1, imagePath.lastIndexOf('?'));
+                    this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+                });
+        } else {
+            var currentName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
+            var correctPath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
+            this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+        }
+    });
+
+    
+ 
+}
+
+createFileName() {
+  var d = new Date(),
+      n = d.getTime(),
+      newFileName = n + ".jpg";
+  return newFileName;
+}
+
+copyFileToLocalDir(namePath, currentName, newFileName) {
+  this.file.copyFile(namePath, currentName, this.file.dataDirectory, newFileName).then(success => {
+      this.updateStoredImages(newFileName);
+  }, error => {
+      this.presentToast('Error while storing file.');
+  });
+}
+
+updateStoredImages(name) {
+  this.storage.get(STORAGE_KEY).then(images => {
+      let arr = JSON.parse(images);
+      if (!arr) {
+          let newImages = [name];
+          this.storage.set(STORAGE_KEY, JSON.stringify(newImages));
+      } else {
+          arr.push(name);
+          this.storage.set(STORAGE_KEY, JSON.stringify(arr));
+      }
+
+      let filePath = this.file.dataDirectory + name;
+      let resPath = this.pathForImage(filePath);
+
+      let newEntry = {
+          name: name,
+          path: resPath,
+          filePath: filePath
+      };
+
+      this.images = [newEntry, ...this.images];
+      this.ref.detectChanges(); // trigger change detection cycle
+  });
+}
+deleteImage(imgEntry, position) {
+  this.images.splice(position, 1);
+
+  this.storage.get(STORAGE_KEY).then(images => {
+      let arr = JSON.parse(images);
+      let filtered = arr.filter(name => name != imgEntry.name);
+      this.storage.set(STORAGE_KEY, JSON.stringify(filtered));
+
+      var correctPath = imgEntry.filePath.substr(0, imgEntry.filePath.lastIndexOf('/') + 1);
+
+      this.file.removeFile(correctPath, imgEntry.name).then(res => {
+          this.presentToast('File removed.');
+      });
+  });
+}
 
 }
